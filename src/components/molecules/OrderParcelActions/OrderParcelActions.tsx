8880c158b56e182023824d53dcfe7ede229e3726
pfulara <fulara.pawel@gmail.com>
2025-06-27 10:31:43 +0200

    fix return request

--- src/components/molecules/OrderParcelActions/OrderParcelActions.tsx
+++ src/components/molecules/OrderParcelActions/OrderParcelActions.tsx
@@ -3,6 +3,7 @@ import { OrderReturn } from "@/components/cells/OrderReturn/OrderReturn"
 
 export const OrderParcelActions = ({ order }: { order: any }) => {
   // if (order.status === "pending") return <OrderCancel order={order} />
-  if (order.status === "completed") return <OrderReturn order={order} />
+  if (order.status === "completed" || order.fulfillment_status === "delivered")
+    return <OrderReturn order={order} />
   return null
 }
